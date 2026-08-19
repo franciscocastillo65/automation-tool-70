926c82f16cf681255e816b24667b78959aca4d3f
Francisco Castillo <franciscocastillo6531@gmail.com>
2026-08-19 09:17:46 +0200

    fix threading for click performance

--- core.py
+++ core.py
@@ -1,34 +1,30 @@
 import time
-from threading import Thread
+import threading
 
 class AutoClicker:
-    def __init__(self, interval=1):
+    def __init__(self, interval):
         self.interval = interval
         self.running = False
 
     def start(self):
-        self.running = True
-        self._run()
-
-    def _run(self):
-        while self.running:
-            self.click()
-            time.sleep(self.interval)
-
-    def click(self):
-        print("Click!")  # Placeholder for actual click code
+        if not self.running:
+            self.running = True
+            threading.Thread(target=self._click_loop, daemon=True).start()
 
     def stop(self):
         self.running = False
 
+    def _click_loop(self):
+        while self.running:
+            self._perform_click()
+            time.sleep(self.interval)
 
-def create_clicker(interval):
-    return AutoClicker(interval)
+    def _perform_click(self):
+        # Simulation of click action
+        print('Click')
 
-
-def start_clicker(clicker):
-    Thread(target=clicker.start).start()
-
-
-def stop_clicker(clicker):
-    clicker.stop()
+if __name__ == '__main__':
+    clicker = AutoClicker(0.1)
+    clicker.start()
+    time.sleep(1)
+    clicker.stop()  
